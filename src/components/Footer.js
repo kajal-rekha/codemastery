c044edb2b9f38069d87d796c6e1174180de7f877
kajal-2011 <kajalrekha888@gmail.com>
2023-05-11 17:30:24 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="text-center py-10 mt-28 bg-teal-700">
+      <p className="text-lg ">
+        &copy; {new Date().getFullYear()} Codemastery. All rights reserved
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
