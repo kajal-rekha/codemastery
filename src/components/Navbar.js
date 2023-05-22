c221f378fe6fc22deb6bfcb487f108e9e73c26d5
kajal-2011 <kajalrekha888@gmail.com>
2023-05-23 04:08:38 +0600

    nav toggle added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+import { useCallback } from "react";
 
 const Navbar = () => {
+  const [toggleOpen, setToggleOpen] = useState(false);
+
+  const handleToggle = useCallback(() => {
+    if (window.innerWidth > 960) {
+      return;
+    }
+
+    setToggleOpen(!toggleOpen);
+    document.body.classList.toggle("lock-scroll");
+  }, [toggleOpen]);
+
   return (
-    <div className="navbar container mx-auto lg:h-24 h-28 flex flex-col  justify-center lg:flex-row lg:justify-between items-center border-b border-teal-900 ">
-      <div className="left flex">
+    <div className="navbar container mx-auto h-20 flex   justify-between items-center border-b border-teal-900">
+      <div className="logo">
         <span className="text-2xl text-gray-100 font-semibold">
           Code<span className="text-teal-500">mastery</span>
         </span>
       </div>
 
       <div className="right">
-        <nav className=" flex items-center gap-5 text-gray-100 font-medium text-[1rem]">
+        <nav
+          className={`${
+            toggleOpen
+              ? "flex flex-col fixed z-[2] w-screen h-[calc(screen-5rem)] top-0 bottom-0 left-0 right-0 items-center justify-center text-lg gap-5 text-center backdrop-blur-md"
+              : "hidden lg:flex gap-5"
+          } `}
+        >
           <Link to="/" className="nav-link hover:text-teal-500 duration-300">
             Home
           </Link>
@@ -62,14 +83,30 @@ const Navbar = () => {
         </nav>
       </div>
 
-      <div className="text-gray-100 font-medium text-[1rem]">
+      <div className="subscribe">
         <a
           href="https://www.youtube.com/@spectra.shohan"
-          className=" hover:text-teal-500 hover:underline hover:underline-offset-2 duration-300"
+          className=" text-lg hover:text-teal-500 hover:underline hover:underline-offset-2 duration-300"
         >
           Subscribe
         </a>
       </div>
+      <button className="z-[3] lg:hidden">
+        <span>
+          <FiMenu
+            onClick={handleToggle}
+            className={`${
+              !toggleOpen ? "block" : "hidden"
+            } text-xl lg:hidden cursor-pointer`}
+          />
+          <AiOutlineClose
+            onClick={handleToggle}
+            className={`${
+              toggleOpen ? "block" : "hidden"
+            } text-xl lg:hidden cursor-pointer`}
+          />
+        </span>
+      </button>
     </div>
   );
 };
